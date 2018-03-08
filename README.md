55cf7ebbb2da00828bc1b144c34db25cdd84aa1c
Bruno HALEBLIAN <bruno_haleblian@carrefour.com>
2018-03-08 23:41:27 +0100

    Beautify and add missing explicit dependency

--- README.md
+++ README.md
@@ -7,7 +7,6 @@ Requirements :
 See each folder for custom setup
 
 NOTICE : terraform parallelism may still be buggy.
-If you run into problems requiring you to run "terraform apply" twice or more,
-try "terraform apply -parallelism=1"
+If you run into problems requiring you to run "terraform apply" twice or more, it is likely some depends_on are missing.
 
 Have fun!
--- labOne/07-floatingIP.tf
+++ labOne/07-floatingIP.tf
@@ -1,8 +1,8 @@
 resource "openstack_networking_floatingip_v2" "terraform_floatip" {
-pool = "${var.poolFIP}"
+  pool = "${var.poolFIP}"
 }
 
 resource "openstack_compute_floatingip_associate_v2" "terraform_floatip" {
-floating_ip = "${openstack_networking_floatingip_v2.terraform_floatip.address}"
-instance_id = "${openstack_compute_instance_v2.terraform_test_instance.id}"
+  floating_ip = "${openstack_networking_floatingip_v2.terraform_floatip.address}"
+  instance_id = "${openstack_compute_instance_v2.terraform_test_instance.id}"
 }
