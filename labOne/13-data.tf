cd09289cb12625cee0bb6c8a4ee7228a48627a4a
Bruno HALEBLIAN <bruno_haleblian@carrefour.com>
2018-04-18 18:25:36 +0200

    Guess flavor

--- labOne/13-data.tf
+++ labOne/13-data.tf
@@ -3,7 +3,7 @@ data "openstack_networking_network_v2" "terraform_external_network" {
 }
 
 data "openstack_compute_flavor_v2" "terraform_best_flavor" {
-  min_ram  = 2048
+  ram  = 2048
   min_disk = 20
-  vcpus    = 2
+  vcpus    = 1
 }
