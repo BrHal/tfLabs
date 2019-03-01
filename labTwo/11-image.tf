37394672aab2cb8c9bf7e9e0b283496484e968d6
Bruno HALEBLIAN <bruno_haleblian@carrefour.com>
2019-03-02 00:45:36 +0100

    Inject metadata to upstream image

--- labTwo/11-image.tf
+++ labTwo/11-image.tf
@@ -3,6 +3,16 @@ resource "openstack_images_image_v2" "terraform_upstream_image" {
   image_source_url = "${var.imageURLs["${var.operatingSystem}"]}"
   container_format = "bare"
   disk_format      = "qcow2"
+
+  properties = {
+    hw_vif_multiqueue_enabled = "true"
+    os_distro                 = "${var.operatingSystem}"
+    hw_watchdog_action        = "reset"
+    hw_qemu_guest_agent       = "yes"
+
+    #    hw_scsi_model             = "virtio-scsi"
+    #    hw_disk_bus               = "scsi"
+  }
 }
 
 data "openstack_images_image_v2" "terraform_tenant_image" {
